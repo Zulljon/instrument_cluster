d9c27e52b24f0f9e0030c17d745425b861d05bc8
zulljon <zulljondan@gmail.com>
2017-11-24 12:19:58 +0200

    update graphics for readme file

---
+++ graphics.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mv -f "instrument_cluster.svg" "instrument_cluster.sch.svg"
+mv -f "2can_filter-Module A (2can_filter).svg" "2can_filter.sch.svg"
+mv -f "2can_filter+extra-Module B (2can_filter+extra).svg" "2can_filter+extra.sch.svg"
+mv -f "power_supply-power_supply.svg" "power_supply.sch.svg"
+
+exit 0
